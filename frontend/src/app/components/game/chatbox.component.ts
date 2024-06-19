7043dfb53b8e068998f13528605412928cba1da2
vivientxy <vivientxy@hotmail.com>
2024-06-20 00:45:57 +0800

    added css for main game component

--- frontend/src/app/components/game/chatbox.component.ts
+++ frontend/src/app/components/game/chatbox.component.ts
@@ -9,7 +9,30 @@ import { WebSocketService } from '../../services/websocket.service';
 export class ChatboxComponent implements OnInit {
   
   message: string = '';
-  messages: string[] = [];
+  messages: string[] = ["vivtxy: A",
+                        "kitkiat: B",
+                        "bendevon: UP",
+                        "leslietangweejie: UP",
+                        "kitkiat: SELECT",
+                        "vivtxy: B",
+                        "bendevon: START",
+                        "leslietangweejie: L",
+                        "kitkiat: A",
+                        "leslietangweejie: A",
+                        "vivtxy: UP",
+                        "username: UP",
+                        "kitkiat: SELECT",
+                        "vivtxy: B",
+                        "bendevon: START",
+                        "username: L",
+                        "username: A",
+                        "username: UP",
+                        "username: UP",
+                        "username: SELECT",
+                        "username: B",
+                        "username: START"];
+
+  
 
   constructor(private wsService: WebSocketService) {}
 
