6323caa1affd2dadef1e755861d0c756a612a12a
vivientxy <vivientxy@hotmail.com>
2024-06-30 21:47:01 +0800

    added /upgrade/fail page

--- frontend/src/app/components/membership/fail.component.ts
+++ frontend/src/app/components/membership/fail.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Membership, MEMBERSHIPS } from '../../models/membership.model';
+import { User } from '../../models/user.model';
+import { StripeService } from '../../services/stripe.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-fail',
@@ -6,5 +11,24 @@ import { Component } from '@angular/core';
   styleUrl: './fail.component.css'
 })
 export class FailComponent {
+  
+  private readonly userSvc = inject(UserService)
+  private readonly stripeSvc = inject(StripeService)
+  private readonly router = inject(Router)
+  user!: User;
+
+  ngOnInit(): void {
+    let user = this.userSvc.getUser();
+    let uuid = sessionStorage.getItem('uuid');
+
+    if (user && uuid) {
+      this.user = user
+    } else {
+      this.router.navigate(['/'])
+      return
+    }
+    
+    // sessionStorage.removeItem('uuid');
+  }
 
 }
